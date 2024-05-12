9abb3f69a11b153ba2880643160f17117d8bfb9c
jorgeaveraf <jorgeaveraf@gmail.com>
2024-05-12 12:26:02 -0600

    Project initiation

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+import Login from './Login';
+import { Route, Routes } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Routes>
+            <Route path="/" element={<LinkList/>} />
+            <Route
+              path="/create"
+              element={<CreateLink/>}
+            />
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants';
+
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+  headers: {
+  ...headers,
+  authorization: token ? `JWT ${token}` : ''
+  }
+  };
+  });
+// 3
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+  });
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
